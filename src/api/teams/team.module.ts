04bbc982a0b784982e56ac33ea2938174859e8ec
No-sign <55203083+No-sign@users.noreply.github.com>
2022-06-26 22:55:48 +0700

    fixed team module

--- src/api/teams/team.module.ts
+++ src/api/teams/team.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { LineUp } from "src/model/sql-entity/lineUp.entity";
 import { Team } from "src/model/sql-entity/team.entity";
+import { LineUpController } from "./lineUp/lineUp.controller";
 import { LineUpService } from "./lineUp/lineUp.service";
 import { TeamController } from "./team.controller";
 import { TeamService } from "./team.service";
@@ -10,7 +11,7 @@ import { TeamService } from "./team.service";
   imports: [
     TypeOrmModule.forFeature([ Team, LineUp ])
   ],
-  controllers: [ TeamController, LineUpService ],
+  controllers: [ TeamController, LineUpController ],
   providers: [ TeamService, LineUpService ]
 })
 export class TeamModule { }
