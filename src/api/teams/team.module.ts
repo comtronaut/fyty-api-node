0d675f4384e5b73f38f5758db606fd4d55334d4d
No-sign <55203083+No-sign@users.noreply.github.com>
2022-07-31 14:48:46 +0700

    import entities and service

--- src/api/teams/team.module.ts
+++ src/api/teams/team.module.ts
@@ -1,19 +1,21 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { TeamLineUp } from "src/model/sql-entity/team/lineUp.entity";
+import { TeamPending } from "src/model/sql-entity/team/pending.entity";
 import { TeamMember } from "src/model/sql-entity/team/team-member.entity";
 import { Team } from "src/model/sql-entity/team/team.entity";
 import { LineUpController } from "./lineUp/lineUp.controller";
 import { LineUpService } from "./lineUp/lineUp.service";
 import { TeamMemberService } from "./members/team-member.service";
+import { TeampendingService } from "./pending/teampending.service";
 import { TeamController } from "./team.controller";
 import { TeamService } from "./team.service";
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([ Team, TeamLineUp,TeamMember])
+    TypeOrmModule.forFeature([ Team, TeamLineUp, TeamMember, TeamPending])
   ],
   controllers: [ TeamController, LineUpController ],
-  providers: [ TeamService, LineUpService,TeamMemberService ]
+  providers: [ TeamService, LineUpService, TeamMemberService, TeampendingService ]
 })
 export class TeamModule { }
