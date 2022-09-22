de561c145ecebda0d378858909c4b0a39dc1f53e
No-sign <55203083+No-sign@users.noreply.github.com>
2022-09-22 21:19:39 +0700

    add service stat

---
+++ src/api/teams/statistic/matchHistory.service.ts
@@ -0,0 +1,29 @@
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { TeamStatistic } from "src/model/sql-entity/team/statistic/stat.entity";
+import { Team } from "src/model/sql-entity/team/team.entity";
+import { Repository } from "typeorm";
+
+@Injectable()
+export class MatchHistortService {
+  constructor(
+    @InjectRepository(Team) private teamModel: Repository<Team>,
+    @InjectRepository(TeamStatistic) private teamStatModel: Repository<TeamStatistic>,
+  ) { }
+
+    // CRUD
+  
+    async getTeamStat(teamId: string){
+        try {
+          return await this.teamStatModel.findOneByOrFail({ id: teamId });
+        } 
+        catch(err) {
+            throw new BadRequestException(err.message);
+        }
+    }
+
+    async createStat(teamId: string){
+        
+    }
+
+}
