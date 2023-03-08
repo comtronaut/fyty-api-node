09f06f91647a59a543c232ebad4253a72aa55e79
phubet16132 <phu-ruji@hotmail.com>
2023-03-08 10:26:02 +0700

    games service tested

--- src/api/games/game.service.ts
+++ src/api/games/game.service.ts
@@ -20,15 +20,11 @@ export class GameService {
 
   async update(gameId: string, req: object) {
     try {
-      const updateRes = await this.prisma.game.update({
+      return await this.prisma.game.update({
         where: {
           id: gameId
         },
         data: req
-      });
-
-      return await this.prisma.game.findUniqueOrThrow({
-        where: { id: gameId }
       });
     } catch (err) {
       throw new BadRequestException(err.message);
@@ -37,8 +33,7 @@ export class GameService {
 
   async delete(gameId: string) {
     try {
-      const res = await this.prisma.game.delete({ where: { id: gameId } });
-      return;
+      return await this.prisma.game.delete({ where: { id: gameId } });
     } catch (err) {
       throw new BadRequestException(err.message);
     }
