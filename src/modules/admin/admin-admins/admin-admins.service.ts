82559cb27b1f6b7e426bfc46b007ac23cb135034
Apisit Ritreungroj <apisit_lon@hotmail.com>
2023-08-13 15:08:41 +0700

    update: admin not return password

--- src/modules/admin/admin-admins/admin-admins.service.ts
+++ src/modules/admin/admin-admins/admin-admins.service.ts
@@ -1,37 +1,41 @@
 import { Injectable } from "@nestjs/common";
-import { Admin } from "@prisma/client";
 import * as bcrypt from "bcrypt";
 
-import { CreateAdminDto, UpdateAdminDto } from "model/dto/admin.dto";
+import { CreateAdminDto, SecureAdminDto, UpdateAdminDto } from "model/dto/admin.dto";
 import { PrismaService } from "prisma/prisma.service";
 
 @Injectable()
 export class AdminAdminsService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async createAdmin(payload: CreateAdminDto): Promise<Admin> {
-    const adminData = payload;
+  async createAdmin(payload: CreateAdminDto): Promise<SecureAdminDto> {
     const hashedPassword = await bcrypt.hash(payload.password, 12);
-    adminData.password = hashedPassword;
 
-    return await this.prisma.admin.create({
-      data: adminData
-    });
-  }
-
-  async getAllAdmin(): Promise<Admin[]> {
-    return await this.prisma.admin.findMany();
-  }
-
-  async getAdminById(adminId: string): Promise<Admin> {
-    return await this.prisma.admin.findUniqueOrThrow({
-      where: {
-        id: adminId
+    const { password, ...data } = await this.prisma.admin.create({
+      data: {
+        ...payload,
+        password: hashedPassword
       }
     });
+
+    return data;
   }
 
-  async updateAdminData(adminId: string, payload: UpdateAdminDto): Promise<Admin> {
+  async getAllAdmin(): Promise<SecureAdminDto[]> {
+    const admins = await this.prisma.admin.findMany();
+
+    return admins.map(({ password, ...data }) => data);
+  }
+
+  async getAdminById(adminId: string): Promise<SecureAdminDto> {
+    const { password, ...data } = await this.prisma.admin.findUniqueOrThrow({
+      where: { id: adminId }
+    });
+
+    return data;
+  }
+
+  async updateAdminData(adminId: string, payload: UpdateAdminDto): Promise<SecureAdminDto> {
     const adminData = payload;
 
     if (payload.password) {
@@ -39,19 +43,19 @@ export class AdminAdminsService {
       adminData.password = hashedPassword;
     }
 
-    return await this.prisma.admin.update({
+    const { password, ...data } = await this.prisma.admin.update({
       where: {
         id: adminId
       },
       data: adminData
     });
+
+    return data;
   }
 
   async deleteAdmin(adminId: string): Promise<void> {
     await this.prisma.admin.delete({
-      where: {
-        id: adminId
-      }
+      where: { id: adminId }
     });
   }
 }
